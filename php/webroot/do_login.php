53af3fa93047e4bd17b272f40f1fa2c6beb0665d
Foxclip <destroyApp2@gmail.com>
2025-04-09 01:54:20 +1000

    При входе вызывается trim() для логина

--- php/webroot/do_login.php
+++ php/webroot/do_login.php
@@ -5,6 +5,8 @@ require_once "utils.php";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     checkPostVariables(['login', 'password']);
+    $login = trim($_POST['login']);
+    $password = $_POST['password'];
 } else {
     exit();
 }
@@ -12,25 +14,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 // проверяем существует ли пользователь с таким логином
 $pdo = pdo_connect_mysql();
 $stmt = $pdo->prepare('SELECT * FROM users WHERE username = :username');
-$stmt->execute(['username' => $_POST['login']]);
+$stmt->execute(['username' => $login]);
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 if (!$user) {
-    echo 'Пользователь ' . $_POST['login'] . ' не зарегистрирован';
+    echo 'Пользователь ' . $login . ' не зарегистрирован';
     exit();
 }
 
 // проверяем пароль
-if (password_verify($_POST['password'], $user['password'])) {
+if (password_verify($password, $user['password'])) {
     if (password_needs_rehash($user['password'], PASSWORD_DEFAULT)) {
-        $newHash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+        $newHash = password_hash($password, PASSWORD_DEFAULT);
         $stmt = $pdo->prepare('UPDATE users SET password = :password WHERE username = :username');
         $stmt->execute([
-            'username' => $_POST['login'],
+            'username' => $login,
             'password' => $newHash,
         ]);
-        $_SESSION['user_id'] = $user['id'];
     }
-    echo 'Пользователь ' . $_POST['login'] . ' успешно авторизован';
+    echo 'Пользователь ' . $login . ' успешно авторизован';
 } else {
     echo 'Неправильный пароль';
     exit();
