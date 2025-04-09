69f0c25a931771801a9dd83af180ca6632d23439
Foxclip <destroyApp2@gmail.com>
2025-04-10 02:12:35 +1000

    Добавлена страница создания поста

---
+++ php/src/public/create_post.php
@@ -0,0 +1,32 @@
+<?php
+    require_once('../session.php');
+    require_once('../utils.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<?php
+    includeFile('../head.php', [
+        'title' => 'Создать пост',
+        'styles' => [
+            'common.css',
+            'header.css',
+            'footer.css',
+            'create_post.css'
+        ]
+    ]);
+?>
+<body>
+    <?= includeFile('../header.php'); ?>
+    <div id="createPostContainer">
+        <h1 id="createPostTitle">Создать пост</h1>
+        <form id="createPostForm" action="/do_create_post" method="post">
+            <input id="titleInput" class="formInput" type="text" name="title" placeholder="Заголовок" required />
+            <input id="contentInput" class="formInput" type="text" name="content" placeholder="Текст" required />
+            <button id="createPostButton" type="submit">Отправить</button>
+        </form>
+    </div>
+    <?= includeFile('../footer.php'); ?>
+    <script src="js/create_post.js" type="module"></script>
+</body>
+</html>
