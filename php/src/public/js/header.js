080cfda57f4ddbd01668f0eee18c25caf941249c
Foxclip <destroyApp2@gmail.com>
2025-04-11 18:12:11 +1000

    Проверять существование userMenuButton и userMenuPopup в шапке

--- php/src/public/js/header.js
+++ php/src/public/js/header.js
@@ -1,8 +1,11 @@
 let userMenuButton = document.getElementById("userMenuButton");
 let userMenuPopup = document.getElementById("userMenuPopup");
-userMenuButton.addEventListener("click", () => userMenuPopup.style.display = userMenuPopup.style.display === "block" ? "none" : "block");
-document.addEventListener("click", (event) => {
-    if (!userMenuButton.contains(event.target) && !userMenuPopup.contains(event.target)) {
-        userMenuPopup.style.display = "none";
-    }
-});
+
+if (userMenuButton !== null && userMenuPopup !== null) {   
+    userMenuButton.addEventListener("click", () => userMenuPopup.style.display = userMenuPopup.style.display === "block" ? "none" : "block");
+    document.addEventListener("click", (event) => {
+        if (!userMenuButton.contains(event.target) && !userMenuPopup.contains(event.target)) {
+            userMenuPopup.style.display = "none";
+        }
+    });
+}
