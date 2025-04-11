07ce275648e1908d14800934981323a2228a40c5
Foxclip <destroyApp2@gmail.com>
2025-04-11 21:57:02 +1000

    Валидировать текст в постах

--- php/src/ui/post.php
+++ php/src/ui/post.php
@@ -4,13 +4,13 @@
     <span class="postDatetime"><?=$postDatetime?></span>
 </div>
 <div class="postTitleContainer">
-    <h2 class="postTitle"><?=$postTitle?></h2>
+    <h2 class="postTitle"><?=htmlspecialchars($postTitle)?></h2>
     <!-- <div class="postTags">
         <div class="postTag">Тег 1</div>
         <div class="postTag">Тег 2</div>
     </div> -->
 </div>
-<div class="postContent"><?=$postContent?></div>
+<div class="postContent"><?=nl2br(htmlspecialchars($postContent))?></div>
 <div class="bottomPanel">
     <div class="postLikes <?= $postLikedByUser ? 'active' : '' ?>" data-post-id="<?=$postId?>">
         <div class="postLikesHeart"></div>
