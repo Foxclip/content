ce2d614423194e73d8df7eeef9640d2a45b66a76
Foxclip <destroyApp2@gmail.com>
2025-04-14 18:28:12 +1000

    head.php: выводить csrf токен в meta только если он установлен в $_SESSION

--- php/src/ui/head.php
+++ php/src/ui/head.php
@@ -1,7 +1,9 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <?php if (isset($_SESSION['csrf_token'])): ?>
     <meta name="csrf-token" content="<?php echo $_SESSION['csrf_token']; ?>" />
+    <?php endif; ?>
     <title><?php echo $title; ?></title>
     <?php
     foreach ($styles as $style) {
