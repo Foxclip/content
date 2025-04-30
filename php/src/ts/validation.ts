09f0d1569fcbdf60fb8a488fab2694bad4bb6ee7
Foxclip <destroyApp2@gmail.com>
2025-04-30 21:25:13 +1000

    validation.js: переход с javascript на typescript

--- php/src/ts/validation.ts
+++ php/src/ts/validation.ts
@@ -1,11 +1,11 @@
-export function validateEmail(email) {
+export function validateEmail(email: string) {
     if (!email.match(/^\S+@\S+$/)) {
         return "Некорректный email";
     }
     return '';
 }
 
-export function validatePassword(password) {
+export function validatePassword(password: string) {
     if (password.length < 6 || password.length > 20) {
         return "Пароль должен содержать от 6 до 20 символов";
     }
